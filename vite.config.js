3e55080553da4bf13ed4797d43317a2e90143083
Ananth0005 <124020228+Ananth005@users.noreply.github.com>
2025-12-26 16:10:16 +0530

    for deployement

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'  // ← ADD THIS
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   resolve: {              // ← ADD THIS BLOCK
     alias: {
